12106b60a8a1e9c87eca749839e1d28207ff6f59
andresemiliano1 <andresemiliano1@gmail.com>
2022-11-27 17:22:19 +0100

    Initial commit

---
+++ NIF.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class NIF {
+    private int dni;
+    private char letra;
+
+    //Constructores
+    public NIF() {}
+    public NIF(int dni) {this.dni = dni; letra = calcularLetra();}
+    //Método para calcular la letra del NIF
+    private char calcularLetra() {
+        char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        return letras[dni % 23];
+    }
+    //Método para leer por teclado el número de DNI y calcular la letra
+    public void leer() {
+        Scanner sc = new Scanner(System.in);
+        do {System.out.print("Introduce dni: ");
+            dni = sc.nextInt();}
+        while (dni <= 0);
+        letra = calcularLetra();
+
+    }
+    //Método que devuelve un String de la formato 99999999-X
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(dni);
+        sb.append("-");
+        sb.append(letra);
+        return sb.toString();
+    }
+
+}
